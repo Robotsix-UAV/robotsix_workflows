229d357d3ac10df849c66c3db3eccb9ce323528c
Damien SIX <damien@robotsix.net>
2024-09-06 12:42:13 +0200

    docker with tmux session running

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-# setup ros2 environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/ros_ws/install/setup.bash" --
-exec "$@"
+# Setup ROS2 environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/ros_ws/install/setup.bash"
+
+# Start a tmux session named 'uav_session'
+  tmux new-session -d -s uav_session
+
+# Keep the container running
+tail -f /dev/null
