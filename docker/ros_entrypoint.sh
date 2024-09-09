24c797c018dba55d3aa7f9e16294cd272d28ad02
Damien SIX <damien@robotsix.net>
2024-09-09 13:34:17 +0200

    handle case where python-requirements.txt does not exist

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -6,7 +6,4 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/ros_ws/install/setup.bash"
 
 # Start a tmux session named 'uav_session'
-  tmux new-session -d -s uav_session
-
-# Keep the container running
-tail -f /dev/null
+tmux new-session -s uav_session
